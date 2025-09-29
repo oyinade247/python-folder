acb4235dd9f105f1b36e0464dc1c2d029961a0eb
Oyin <adedokunoyinade@gmail.com>
2025-09-29 14:50:51 +0100

    chapter 3

--- python chapter3 assignment/mileage.py
+++ python chapter3 assignment/mileage.py
@@ -4,18 +4,18 @@ sum_gallon = 0
 
 sum_miles = 0
 
-while(gallon != 0 ):
+while(gallon != -1 ):
 	gallon = int(input("Enter the gallons used(-1 to end):"))
 	sum_gallon += gallon
 
 	miles = int(input("Enter the miles driven: "))
 	sum_miles +=miles
 
-	each_trip = miles // gallon
+	each_trip = miles / gallon
 	print("The miles per gallon for each trip is", each_trip)
 
-	if(gallon < 0):
-		total_trip = sum_gallon // sum_miles 
+	if(gallon == -1):
+		total_trip = sum_gallon / sum_miles 
 		print("The total miles per gallon for each trip is", total_trip)
 
 
